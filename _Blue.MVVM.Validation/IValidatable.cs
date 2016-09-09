264b5621d8295d9b35462cbdc673e7e756893d75
earloc <earloc@live.de>
2016-09-09 14:24:12 +0200

    added interfaces

---
+++ _Blue.MVVM.Validation/IValidatable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blue.MVVM.Validation {
+    public partial interface IValidatable {
+        event EventHandler IsValidChanged;
+        bool IsValid { get; }
+
+        void SetError(string propertyName, string errorText = null);
+        void RemoveError(string propertyName);
+    }
+}
---
+++ _Blue.MVVM.Validation/IValidatableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+#if !PRE_45
+using System.Runtime.CompilerServices;
+#endif
+using System.Text;
+
+namespace Blue.MVVM.Validation {
+    public static class IValidatableExtensions {
+
+        public static void ValidatesWhen(this IValidatable source, 
+            bool condition,
+#if !PRE45
+            [CallerMemberName]
+#endif
+            string propertyName = "", 
+            string errorText = "") {
+            if (condition) {
+                source.RemoveError(propertyName);
+                return;
+            }
+            source.SetError(propertyName, errorText);
+        }
+    }
+}
